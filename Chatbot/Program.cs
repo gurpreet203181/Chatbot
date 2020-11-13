15d3c99a54303153809bf570680ddec5559ee775
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-13 13:02:55 +0100

    Add ciclo switch

--- Chatbot/Program.cs
+++ Chatbot/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Chatbot
 {
@@ -10,6 +9,89 @@ namespace Chatbot
     {
         static void Main(string[] args)
         {
+            // Lister: in ascolto quando si parla dei server
+            // EndPoint: identifica una coppia IP/Porta
+
+            //Creare il mio socketlistener
+            //1) specifico che versione IP
+            //2) tipo di socket. Stream.
+            //3) protocollo a livello di trasporto
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+                                    ProtocolType.Tcp);
+
+
+            // config: IP dove ascoltare. Possiamo usare l'opzione Any: ascolta da tutte le interfaccie all'interno del mio pc.
+            IPAddress ipaddr = IPAddress.Any;
+            // config: devo configurare l'EndPoint
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
+
+            // config: Bind -> collegamento
+            // listenerSocket lo collego all'endpoint che ho appena configurato
+            listenerSocket.Bind(ipep);
+            // Mettere in ascolto il server.
+            // parametro: il numero massimo di connessioni da mettere in coda.
+            listenerSocket.Listen(5);
+            Console.WriteLine("Server in ascolto...");
+            Console.WriteLine("in attesa di connessione da parte del client...");
+
+            // Istruzione bloccante
+            // restituisce una variabile di tipo socket.
+            Socket client = listenerSocket.Accept();
+
+            Console.WriteLine("Client IP: " + client.RemoteEndPoint.ToString());
+
+            // mi attrezzo per ricevere un messaggio dal client
+            // siccome è di tipo stream io riceverò dei byte, o meglio un byte array
+            // riceverò anche il numero di byte.
+            byte[] buff = new byte[128];
+            int receivedBytes = 0;
+            int sendedBytes = 0;
+            string receivedString, sendString;
+
+            while (true)
+            {
+                receivedBytes = client.Receive(buff);
+                Console.WriteLine("Numero di byte ricevuti: " + receivedBytes);
+                receivedString = Encoding.ASCII.GetString(buff, 0, receivedBytes);
+                Console.WriteLine("Stringa ricevuta: " + receivedString);
+                sendString = "Benvenuto client";
+                Array.Clear(buff, 0, buff.Length);
+                if (receivedString.ToUpper() == "QUIT")
+                {
+                    break;
+                }
+                switch (receivedString.ToUpper())
+                {
+                    case "CIAO":
+                        // crea il messaggio
+                        sendString = "Ciao Buongiorno";
+                        break;
+
+                   
+                        
+                    default:
+                        // crea il messaggio
+                        
+                        break;
+                }
+
+               
+                sendedBytes = 0;
+
+                // lo converto in byte
+                buff = Encoding.ASCII.GetBytes(sendString);
+
+                //invio al client il messaggio
+                sendedBytes = client.Send(buff);
+
+                Array.Clear(buff, 0, buff.Length);
+
+            }
+            // Termina il programma
+
+
+
+
         }
     }
 }
