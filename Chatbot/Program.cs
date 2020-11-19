aa90d23a8939d9e0e4f4399acc1855c18a20f3d2
gurpreet203181 <45843946+gurpreet203181@users.noreply.github.com>
2020-11-19 20:00:38 +0100

    Update Program.cs

--- Chatbot/Program.cs
+++ Chatbot/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 
-namespace Chatbot
+namespace ChatbotServer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Lister: in ascolto quando si parla dei server
+                  // Lister: in ascolto quando si parla dei server
             // EndPoint: identifica una coppia IP/Porta
 
             //Creare il mio socketlistener
@@ -18,27 +19,29 @@ namespace Chatbot
             //3) protocollo a livello di trasporto
             Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
                                     ProtocolType.Tcp);
-
-
             // config: IP dove ascoltare. Possiamo usare l'opzione Any: ascolta da tutte le interfaccie all'interno del mio pc.
             IPAddress ipaddr = IPAddress.Any;
+
             // config: devo configurare l'EndPoint
             IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
 
             // config: Bind -> collegamento
             // listenerSocket lo collego all'endpoint che ho appena configurato
             listenerSocket.Bind(ipep);
+
             // Mettere in ascolto il server.
             // parametro: il numero massimo di connessioni da mettere in coda.
             listenerSocket.Listen(5);
             Console.WriteLine("Server in ascolto...");
             Console.WriteLine("in attesa di connessione da parte del client...");
-
             // Istruzione bloccante
             // restituisce una variabile di tipo socket.
             Socket client = listenerSocket.Accept();
 
             Console.WriteLine("Client IP: " + client.RemoteEndPoint.ToString());
+           
+            
+            
 
             // mi attrezzo per ricevere un messaggio dal client
             // siccome è di tipo stream io riceverò dei byte, o meglio un byte array
@@ -47,51 +50,79 @@ namespace Chatbot
             int receivedBytes = 0;
             int sendedBytes = 0;
             string receivedString, sendString;
+             bool flag=true;
+            while (true) { 
+              
 
-            while (true)
-            {
                 receivedBytes = client.Receive(buff);
                 Console.WriteLine("Numero di byte ricevuti: " + receivedBytes);
                 receivedString = Encoding.ASCII.GetString(buff, 0, receivedBytes);
                 Console.WriteLine("Stringa ricevuta: " + receivedString);
-                sendString = "Benvenuto client";
-                Array.Clear(buff, 0, buff.Length);
-                if (receivedString.ToUpper() == "QUIT")
+                
+                if (receivedString.ToUpper().Trim() == "QUIT")
                 {
                     break;
                 }
-                switch (receivedString.ToUpper())
-                {
-                    case "CIAO":
-                        // crea il messaggio
-                        sendString = "Ciao Buongiorno";
-                        break;
-
+                if(flag){
+                   sendString = "Benevenuto"  + "\r\n";
+                    flag=false;
                    
-                        
-                    default:
-                        // crea il messaggio
-                        
-                        break;
-                }
+                }else{
 
+                    //metodo per la risposta di bot
+                sendString = RispostaBot(receivedString) + "\r\n";
+                }
                
-                sendedBytes = 0;
+                
+
+
+                
+                   Array.Clear(buff, 0, buff.Length);
+                   sendedBytes = 0;
+               
+                
+                
 
                 // lo converto in byte
                 buff = Encoding.ASCII.GetBytes(sendString);
 
                 //invio al client il messaggio
-                sendedBytes = client.Send(buff);
-
-                Array.Clear(buff, 0, buff.Length);
-
+                sendedBytes=client.Send(buff);
+                   Array.Clear(buff, 0, buff.Length);
+                
             }
             // Termina il programma
 
 
+        }
 
+        public static string  RispostaBot(string receivedString)
+        {
+            string sendString="";
+             switch (receivedString.ToUpper().Trim())
+	             {
+                    case "CIAO":
+                    sendString = "Buongiorno";
+                        break;
+		           case "COME STAI":
+                    sendString = "Bene";
+                        break;
+                        case "CHE FAI":
+                    sendString = "Niente";
+                        break;
+                   
+                    
+                    default:
+                        sendString = " Prova questi comandi: ciao , come stai, Che Fai?, Quit  ";
+                     break;
+	            }
+            return sendString;
+        }
+          
 
         }
+
     }
-}
+
+
+
